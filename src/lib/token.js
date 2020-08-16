a37d0b795d8bcb43b917c30568d6fdda4e7df1d6
TitanV <a01098930999@gmail.com>
2020-08-16 15:37:56 +0900

    add - 토큰제어 모듈 분리

---
+++ src/lib/token.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+exports.jwtsign = (async (id) => {
+  const token = jwt.sign({ id: `${id}` },process.env.secretjwt,{expiresIn: '2h'});
+  return token;
+});
+
+exports.jwtverify = (async (token) => {
+  let check;
+  jwt.verify(token, process.env.secretjwt, (error, decoded) => {
+    if(error){ check = false; }
+    else{
+      check = decoded['id'];
+    }
+  });
+  return check;
+});
