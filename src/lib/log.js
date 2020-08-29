09cf015c596f5d48966e069c06dd9f6354aff313
titanv <a01098930999@gmail.com>
2020-08-30 02:40:44 +0900

    add - log library

---
+++ src/lib/log.js
@@ -0,0 +1,17 @@
+import mariadb from 'mariadb';//mariadb 사용 모듈
+
+const connection = mariadb.createPool({//db 연결용 변수, 내부 변수는 환경변수로 설정.
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+});
+
+exports.setlog = (async (name,id,contents) => {
+  let sql,rows;
+
+  sql = `INSERT log(name,id,contents) VALUES('${name}','${id}','${contents}')`;
+  rows = await connection.query(sql);
+
+  return;
+});
