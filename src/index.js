df8141e481d101e6a6755dbddc478ca2003ee0db
titanv <a01098930999@gmail.com>
2020-08-30 02:43:04 +0900

    update - change route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import Koa  from 'koa';
-import Router  from 'koa-router';
-import logger  from 'koa-logger';
-import koaBody  from 'koa-body';
-import http  from 'http';
+import Koa from 'koa';
+import Router from '@koa/router';
+import logger from 'koa-logger';
+import koaBody from 'koa-body';
+import http from 'http';
 import cors from '@koa/cors';
-import helmet from "koa-helmet";
+import helmet from 'koa-helmet';
 
 import api  from './api';
 import db from './db';
@@ -15,7 +15,8 @@ const router = new Router();
 app.use(helmet());
 app.use(cors());
 app.use(logger());//http 메소드 로거 사용
-app.use(koaBody());//json 형식 파싱용
+app.use(koaBody());
+
 app.use(router.routes()).use(router.allowedMethods());
 
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
