ff2171c286838bc17b7f1306f9365424b248fb33
TitanV <a01098930999@gmail.com>
2020-12-21 16:03:02 +0900

    add - pin gen

--- src/lib/mail-ses.js
+++ src/lib/mail-ses.js
@@ -20,3 +20,11 @@ exports.sendmail = (async (email,mailsubject,contents) => {
     console.log(res);
   });
 });
+
+exports.makecode = (async () => {
+
+  let result = Math.floor(Math.random() * 1000000)+100000;
+  if(result>1000000){ result = result - 100000; }
+
+  return result;
+});
